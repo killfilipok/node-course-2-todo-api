4edf2378a295f76b5e490b4dbf2c5034ab57ea22
philip <killfilipok@gmail.com>
2018-04-14 15:31:11 +0300

    add supertest

---
+++ server/tests/server.test.js
@@ -0,0 +1,51 @@
+const expect = require('expect');
+const reques = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../modles/todo')
+
+beforeEach((done) => {
+  Todo.remove({}).then(() => done())
+})
+
+describe('Post /todos', () => {
+  it('should create a new todo', (done) => {
+    var text = 'Test todo text';
+
+    reques(app)
+    .post('/todos')
+    .send({text})
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.text).toBe(text);
+    })
+    .end((err, res) => {
+      if(err){
+        return done(err)
+      }
+
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(1);
+        expect(todos[0].text).toBe(text);
+        done();
+      }).catch((err) => done(err));
+    })
+  })
+
+  it('should not create todo with invalid body data', (done) => {
+    reques(app)
+    .post('/todos')
+    .send({text: ""})
+    .expect(400)
+    .end((err, res) => {
+      if(err){
+        return done(err)
+      }
+
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(0);
+        done();
+      }).catch((err) => done(err));
+    })
+  });
+});
