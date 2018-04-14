8624690801cd0c29bfa45547c1f23b9c256dbe95
philip <killfilipok@gmail.com>
2018-04-14 20:39:35 +0300

    add text casec todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const reques = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../modles/todo')
 
 const todos = [{
+  _id:  new ObjectID(),
   text: 'First test todo'
 }, {
+  _id:  new ObjectID(),
   text: 'sec test todo'
 }]
 
@@ -71,5 +74,31 @@ describe('GET /todos', () => {
       expect(res.body.todos.length).toBe(2)
     })
     .end(done);
+  })
+})
+
+describe('GET/ todos/:id', () => {
+  it('should return todo doc', (done) => {
+    reques(app)
+    .get(`/todos/${todos[0]._id.toHexString()}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo.text).toBe(todos[0].text);
+    })
+    .end(done)
+  })
+
+  it('should return 404 if todo not found', (done) => {
+    reques(app)
+    .get(`/todos/${new ObjectID().toHexString()}`)
+    .expect(404)
+    .end(done)
+  })
+
+  it('should return 404 for non object ids', (done) => {
+    reques(app)
+    .get('/todos/123')
+    .expect(404)
+    .end(done)
   })
 })
