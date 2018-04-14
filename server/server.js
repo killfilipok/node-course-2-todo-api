fcde4e0645d3eec0d44b186ca69130d9b27a9457
philip <killfilipok@gmail.com>
2018-04-14 18:53:05 +0300

    add test to Get:/todos

--- server/server.js
+++ server/server.js
@@ -18,11 +18,19 @@ app.post('/todos', (req, res) => {
     res.send(doc);
   }, (err) => {
     res.status(400).send(err);
-  })
+  });
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (err) => {
+    res.status(400).send(err);
+  });
 });
 
 app.listen(3000, () => {
   console.log('startet on port 3000');
-})
+});
 
 module.exports = {app};
