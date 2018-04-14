0e5ac9753cf8904ae3c70f32093de76623997ab4
philip <killfilipok@gmail.com>
2018-04-14 23:39:24 +0300

    add delete

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,13 @@
+const {ObjectID} = require('mongodb');
+
+const {User} = require('./../server/modles/user');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/modles/todo');
+
+// Todo.remove({}).then((res) => {
+//   console.log(res);
+// })
+
+Todo.findByIdAndRemove('5ad264bbe36c4b8d63ef3619').then((todo) => {
+  console.log(todo);
+})
